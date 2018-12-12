12284a157e8dfaa27f5a035b78ebeb4cde4c04d2
Matt Wong <mattwong59@gmail.com>
2018-12-11 19:25:29 -0800

    Add home, contact and about pages

---
+++ tutorial-part-three/src/pages/contact.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default () => (
+    <div>
+        <h1>Let's talk! What's your question? Email me at the address below</h1>
+        <p>
+            <a href="mailto:mattwong59@gmail.com">mattwong59@gmail.com</a>
+        </p>
+    </div>
+)
