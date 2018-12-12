1f32c90d97b38341f84e47cdb66a5db38358ff72
Matt Wong <mattwong59@gmail.com>
2018-12-11 21:06:18 -0800

    Import and use Layout component on all 3 pages

--- tutorial-part-three/src/pages/contact.js
+++ tutorial-part-three/src/pages/contact.js
@@ -1,10 +1,11 @@
 import React from "react"
+import Layout from "../components/layout"
 
 export default () => (
-    <div>
+    <Layout>
         <h1>Let's talk! What's your question? Email me at the address below</h1>
         <p>
             <a href="mailto:mattwong59@gmail.com">mattwong59@gmail.com</a>
         </p>
-    </div>
+    </Layout>
 )
--- tutorial-part-three/src/pages/index.js
+++ tutorial-part-three/src/pages/index.js
@@ -1,10 +1,11 @@
 import React from "react"
+import Layout from "../components/layout"
 
 export default () => (
-    <div>
+    <Layout>
         <h1>Hey! I'm building a fake Gatsby site to practice those skills</h1>
         <p>
             This is just some filler text and my lorem plugin isn't working so I'll just keep typing so this can be a little bit longer 
         </p>
-    </div>
+    </Layout>
 )
